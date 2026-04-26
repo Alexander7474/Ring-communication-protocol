6adbb7f609807fedd66a379e117f0cdb3af41287
Alexandre <alexandre.lanternier@outlook.fr>
2026-04-27 00:47:03 +0200

    Claude is shiting me perfect docs

--- driver/init.c
+++ driver/init.c
@@ -11,28 +11,48 @@
 #include "../common/error.h"
 #include "init.h"
 
-int init_inet_listenner(struct sockaddr_in *servd)
+/**
+ * init_inet_listenner - Initialise et met en écoute un socket TCP
+ *
+ * Crée un socket TCP sur INADDR_ANY:PORT avec l'option SO_REUSEADDR,
+ * l'attache et le met en écoute.
+ *
+ * @servd: structure sockaddr_in à initialiser
+ *
+ * Return: descripteur du socket d'écoute
+ */
+int
+init_inet_listenner(struct sockaddr_in *servd)
 {
         servd->sin_family = AF_INET;
         servd->sin_port = htons(PORT);
         servd->sin_addr.s_addr = htonl(INADDR_ANY);
-
-        int newsockd = socket(AF_INET, SOCK_STREAM, 0); // Création de la socket
-
+        int newsockd = socket(AF_INET, SOCK_STREAM, 0);
         int opt = 1;
         setsockopt(newsockd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
         int cc = bind(newsockd, (struct sockaddr *)servd, sizeof(*servd));
         if (cc == -1)
-                FATAL("bind network"); // Erreur à l'attachement
-
+                FATAL("bind network");
         cc = listen(newsockd, 5);
         if (cc == -1)
                 FATAL("listen network");
-
         return newsockd;
 }
 
-int init_unix_listenner(struct sockaddr_un *servcomm)
+/**
+ * init_unix_listenner - Initialise et met en écoute un socket Unix
+ *
+ * Supprime l'éventuel socket Unix résiduel, crée un socket AF_UNIX,
+ * l'attache et le met en écoute. Le chemin du socket est lu depuis
+ * la variable d'environnement DRIVER_SOCKET_PATH si définie, sinon
+ * UNIX_SOCKET_PATH est utilisé.
+ *
+ * @servcomm: structure sockaddr_un à initialiser
+ *
+ * Return: descripteur du socket d'écoute
+ */
+int
+init_unix_listenner(struct sockaddr_un *servcomm)
 {
         char cmd[64] = "rm ";
         strcat(cmd, UNIX_SOCKET_PATH);
@@ -45,16 +65,12 @@ int init_unix_listenner(struct sockaddr_un *servcomm)
                 socket_path = UNIX_SOCKET_PATH;
         strncpy(servcomm->sun_path, socket_path,
                 sizeof(servcomm->sun_path) - 1);
-
-        int newsockcomm =
-                socket(AF_UNIX, SOCK_STREAM, 0); // Création de la socket
+        int newsockcomm = socket(AF_UNIX, SOCK_STREAM, 0);
         cc = bind(newsockcomm, (struct sockaddr *)servcomm, sizeof(*servcomm));
         if (cc == -1)
-                FATAL("bind unix"); // Erreur à l'attachement
-
+                FATAL("bind unix");
         cc = listen(newsockcomm, 5);
         if (cc == -1)
                 FATAL("listen unix");
-
         return newsockcomm;
 }
